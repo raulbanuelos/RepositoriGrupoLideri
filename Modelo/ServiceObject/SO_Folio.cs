688dedf2c5b7c19a7819d58b6757e437e7b70228
M0051722 <M0051722@MXAG-BANUELOS-D>
2017-07-05 16:08:42 -0500

    Se agrega el parámetro isPosteada en el método GetFoliosPromotor, además se documenta el código.

--- Modelo/ServiceObject/SO_Folio.cs
+++ Modelo/ServiceObject/SO_Folio.cs
@@ -152,26 +152,34 @@ namespace Modelo.ServiceObject
         /// <param name="fechaFinal"></param>
         /// <param name="idUsuario"></param>
         /// <returns></returns>
-        public DataSet GetFoliosPromotor(string fechaIncial, string fechaFinal, int idUsuario)
+        public DataSet GetFoliosPromotor(string fechaIncial, string fechaFinal, int idUsuario,int isPosteada)
         {
             try
             {
+                //Declaramos un objeto de tipo DataSet que será el que guarde los resultados de la consulta.
                 DataSet datos = null;
 
-                GrupoLideri_SQL conexion = new GrupoLideri_SQL("data source=74.208.227.248;initial catalog=Juda;persist security info=True;user id=userPixie;password=Bd.KJY64;");
+                //Declaramos un objeto con el cual nos permitira conectarnos hacia la base de datos.
+                GrupoLideri_SQL conexion = new GrupoLideri_SQL();
 
+                //Declaramos un diccionario en el cual guardaremos los parámetros que requiere el procedimiento.
                 Dictionary<string, object> parametros = new Dictionary<string, object>();
 
+                //Agregamos los parámertros necesarios del procedimiento.
                 parametros.Add("fechaInicial", fechaIncial);
                 parametros.Add("fechaFinal", fechaFinal);
                 parametros.Add("idUsuario", idUsuario);
+                parametros.Add("isPosteada", isPosteada);
 
+                //LLamamos al método para ejecutar el procedimiento, el resultado lo guardamos 
                 datos = conexion.EjecutarStoredProcedure("SP_PROMOTOR_GET_FOLIOS", parametros);
 
+                //Retornamos el resultado.
                 return datos;
             }
             catch (Exception)
             {
+                //Si ocurre algún error, retornamos un nulo.
                 return null;
             }
 
