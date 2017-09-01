9cd5524df06ddd7cf454d854bffbd3cb2cea95a3
M0051722 <M0051722@MXAG-BANUELOS-D>
2017-09-01 16:34:55 -0500

    Add comision controller

--- GrupoLideri/Controllers/ComisionesController.cs
+++ GrupoLideri/Controllers/ComisionesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GrupoLideri.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,50 @@ namespace GrupoLideri.Controllers
 {
     public class ComisionesController : Controller
     {
+        #region Promotor
         // GET: Comisiones
         public ActionResult Promotor()
         {
             return View();
         }
 
+        public JsonResult GetComicionPromotor(string fechaIncial, string fechaFinal)
+        {
+            DO_Persona usuarioConectado = (DO_Persona)Session["UsuarioConectado"];
+
+            List<N_Folio_SIAC> listaResultante = new List<N_Folio_SIAC>();
+
+            listaResultante = DataManagerLideri.GetComisionPromotor(usuarioConectado.idPersona, fechaIncial, fechaFinal);
+
+            var jsonResult = Json(listaResultante, JsonRequestBehavior.AllowGet);
+
+            jsonResult.MaxJsonLength = int.MaxValue;
+
+            return jsonResult;
+        }
+
+        #endregion
+
+        #region Gerente
         public ActionResult Gerente()
         {
             return View();
+        } 
+
+        public JsonResult GetComicionGerente(string fechaIncial, string fechaFinal)
+        {
+            DO_Persona usuarioConectado = (DO_Persona)Session["UsuarioConectado"];
+
+            List<N_Folio_SIAC> listaResultante = new List<N_Folio_SIAC>();
+
+            listaResultante = DataManagerLideri.GetComisionGerente(usuarioConectado.idPersona, fechaIncial, fechaFinal);
+
+            var jsonResult = Json(listaResultante, JsonRequestBehavior.AllowGet);
+
+            jsonResult.MaxJsonLength = int.MaxValue;
+
+            return jsonResult;
         }
+        #endregion
     }
 }
